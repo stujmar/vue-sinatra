18efffd5420aea2b3ebf5006a391e0de66b7bd35
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-14 17:36:50 -0600

    added global stying to the App.vue

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -48,6 +48,29 @@ export default {
 </script>
 
 <style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, san-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
