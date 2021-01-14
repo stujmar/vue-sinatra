0c147c2d091e5e01ed57b3fc526a0a7d84cce817
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-13 21:53:43 -0600

    built out Header.vue content

--- hello-vue/src/components/layout/Header.vue
+++ hello-vue/src/components/layout/Header.vue
@@ -1,13 +1,25 @@
 <template>
-    <div></div>
+    <header class="header">
+        <h1>TodoList</h1>
+    </header>
 </template>
 
 <script>
 export default {
-    
+    name: "Header"
 }
 </script>
 
 <style scoped>
+    .header {
+        background: #333;
+        color: white;
+        text-align: center;
+        padding: 10px;
+    }
 
+    .header a {
+        color: white;
+        padding: 0 5px 0 0;
+    }
 </style>
